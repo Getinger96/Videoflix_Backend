dec0700fce277bc5069da208fa98cae34409e491
Getinger96 <erich.getinger@outlook.de>
2025-12-27 22:23:52 +0100

    added gitignore and model for videos

--- videoflix/models.py
+++ videoflix/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Video(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title=models.CharField(max_length=80)
+    descripition=models.CharField(max_length=250)
+    video_file=models.FileField(upload_to='videos/', null=True, blank=True)
--- videoflix/signals.py
+++ videoflix/signals.py
@@ -1,8 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-@receiver(post_save, sender=Video)
+import os
 
+from videoflix.models import Video
+@receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
     if created:
         print('New Video created')
+
+@receiver(post_delete, sender = Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
