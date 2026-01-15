b3fe490993ade40c509e298fbb9b2e311314d59a
Getinger96 <erich.getinger@outlook.de>
2026-01-15 11:21:52 +0100

    working on reset password email

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import RegistrationView, ActivationView,CookieTokenObtainPairView,LogoutView
+from .views import RegistrationView, ActivationView,CookieTokenObtainPairView,LogoutView,CookieRefreshView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='registration'),
     path('activate/<uidb64>/<token>/', ActivationView.as_view(), name='activate'),
     path('login/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', CookieRefreshView.as_view(), name='token_refresh'),
     
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -15,8 +15,31 @@ from rest_framework_simplejwt.views import (
 )
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken , AccessToken
+from django.contrib.auth.decorators import login_not_required, login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    PasswordChangeForm,
+    PasswordResetForm,
+    SetPasswordForm,
+)
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.tokens import default_token_generator
+
 
 User = get_user_model()
+
+class PasswordContextMixin:
+    extra_context = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {"title": self.title, "subtitle": None, **(self.extra_context or {})}
+        )
+        return context
 
 class RegistrationView(CreateAPIView):
     serializer_class = Registrationserializer
@@ -148,3 +171,75 @@ class LogoutView(APIView):
         response.delete_cookie("refresh_token")
         response.data={"detail":"Logout successfully! All Tokens will be deleted. Refresh token is now invalid"}
         return response
+     
+
+
+class CookieRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+         """
+        Handle POST request to invalidate the refresh token.
+        Removes refresh_token cookie and blacklists the token.
+        """
+         refresh_token=request.COOKIES.get("refresh_token")
+         """
+        Convert the token into a RefreshToken object.
+        This allows the token to be blacklisted.
+        """
+         if refresh_token is None:
+            return Response({"detail":"Refresh token not found!"},
+                            status=status.HTTP_400_BAD_REQUEST)
+         serializer=self.get_serializer(data={"refresh":refresh_token})
+         try:
+            serializer.is_valid(raise_exception=True)
+         except:
+             return Response({"detail":"Refresh token invalid!"},
+                            status=status.HTTP_401_UNAUTHORIZED)
+         access_token=serializer.validated_data.get("access")
+         response=Response({"detail":" Token refreshed",'access':access_token})
+         """
+        Save the new access token as an HttpOnly cookie.
+        This cookie replaces the old access token.
+        """
+         response.set_cookie(
+            key= "access_token",
+            value= access_token,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+         return response
+    
+
+@method_decorator(login_not_required, name="dispatch")
+class PasswordResetView(PasswordContextMixin, FormView):
+    email_template_name = "templates/password_reset_subject.html"
+    extra_email_context = None
+    form_class = PasswordResetForm
+    from_email = None
+    html_email_template_name = None
+    subject_template_name = "templates/password_reset_subject.html"
+    success_url = reverse_lazy("password_reset_done")
+    template_name = "registration/password_reset_form.html"
+    title = _("Password reset")
+    token_generator = default_token_generator
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        opts = {
+            "use_https": self.request.is_secure(),
+            "token_generator": self.token_generator,
+            "from_email": self.from_email,
+            "email_template_name": self.email_template_name,
+            "subject_template_name": self.subject_template_name,
+            "request": self.request,
+            "html_email_template_name": self.html_email_template_name,
+            "extra_email_context": self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
+
+
+INTERNAL_RESET_SESSION_TOKEN = "_password_reset_token"
